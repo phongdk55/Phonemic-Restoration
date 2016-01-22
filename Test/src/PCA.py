6f6b9790b29c10ab8880797be85133f0b9811da7
phongdk92 <phongdk92@gmail.com>
2016-01-23 01:32:20 +0900

    Finish x1 = x*w + b

--- Test/src/PCA.py
+++ Test/src/PCA.py
@@ -1,7 +1,9 @@
 import numpy as np
-from sklearn.decomposition import PCA
-
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-pca = PCA(n_components=2)
-pca.fit(X)
-print(pca.explained_variance_ratio_)
+import pyparsing
+print pyparsing.__version__
+#from sklearn.decomposition import PCA
+#import matplotlib.pyplot as plt
+#X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+#pca = PCA(n_components=2)
+#pca.fit(X)
+#print(pca.explained_variance_ratio_)
